9a8fd1375dbb0d48128376e82e9a0bce90af5373
Simone <simonecabral.br@gmail.com>
2024-11-17 18:28:48 -0300

    Controle de fluxo entre dois números

--- src/Contador.java
+++ src/Contador.java
@@ -4,9 +4,9 @@ public class Contador {
     public static void main(String[] args) {
         Scanner terminal = new Scanner(System.in);
 
-        System.out.println("Imprimindo o número 1 ");
+        System.out.println("Digite o primeiro parâmetro");
         int parametroUm = terminal.nextInt();
-        System.out.println("Imprimindo o número 2 ");
+        System.out.println("Digite o segundo parâmetro");
         int parametroDois = terminal.nextInt();
 
         try {
